a2b4cac36ba9341301d5b674e3689c2b01bf8d69
NatanzinS2 <natanfelipe816@gmail.com>
2025-02-06 10:38:18 -0300

    mds

---
+++ config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Erro ao conectar ao MySQL:", err);
+    return;
+  }
+  console.log("Conexão bem-sucedida ao MySQL!");
+  connection.release(); // Libera a conexão após o teste
+});
+
+module.exports = pool;
